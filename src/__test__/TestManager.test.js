d50267818b371693773f059dbcf064b9423784ea
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-07 17:06:25 -0500

    [add] finish the test

--- src/__test__/TestManager.test.js
+++ src/__test__/TestManager.test.js
@@ -19,25 +19,25 @@ describe('Test load test in TestManager', () => {
     ];
 
     test("A test doesn't have description", () => {
-      const data = dataTest.slice();
+      const data = JSON.parse(JSON.stringify(dataTest));
       delete data[1].description;
       expect(() => { const tm = new TestManager(data); }).toThrow("Test doesn't have the structure of GameTest class");
     });
 
     test("A test doesn't have type", () => {
-      const data = dataTest.slice();
+      const data = JSON.parse(JSON.stringify(dataTest));
       delete data[1].type;
       expect(() => { const tm = new TestManager(data); }).toThrow("Test doesn't have the structure of GameTest class");
     });
 
     test("A test doesn't have value", () => {
-      const data = dataTest.slice();
+      const data = JSON.parse(JSON.stringify(dataTest));
       delete data[1].value;
       expect(() => { const tm = new TestManager(data); }).toThrow("Test doesn't have the structure of GameTest class");
     });
 
     test("A test doesn't have params", () => {
-      const data = dataTest.slice();
+      const data = JSON.parse(JSON.stringify(dataTest));
       delete data[1].params;
       expect(() => { const tm = new TestManager(data); }).toThrow("Test doesn't have the structure of GameTest class");
     });
@@ -73,7 +73,7 @@ describe('Test load test in TestManager', () => {
   });
 
   describe('Test run test', () => {
-    const dataBase = [
+    const dataTest = [
       {
         description: 'Hello',
         type: 'equal',
@@ -89,20 +89,41 @@ describe('Test load test in TestManager', () => {
     ];
 
     test('Run test no throw errors', () => {
-      const data = dataBase.slice();
+      const data = JSON.parse(JSON.stringify(dataTest));
       const tm = new TestManager(data);
       expect(() => (tm.run())).not.toThrow();
 
-      const data2 = data.slice();
+      const data2 = JSON.parse(JSON.stringify(dataTest));
       data2[1].value = 2;
       const tm2 = new TestManager(data2);
-      expect(()=> (tm2.run())).not.toThrow();
+      expect(() => (tm2.run())).not.toThrow();
     });
 
-    test('Test output', () => {
-      const data = dataBase.slice();
+    test('Test output all pass', () => {
+      const data = JSON.parse(JSON.stringify(dataTest));
       const tm = new TestManager(data);
+      tm.run();
+      expect(tm.log.logs.length).toBe(2);
+      expect(tm.log.errors.length).toBe(0);
+    });
 
+    test('Test output any fail', () => {
+      const data = JSON.parse(JSON.stringify(dataTest));
+      data[1].value = 2;
+      const tm = new TestManager(data);
+      tm.run();
+      expect(tm.log.logs.length).toBe(1);
+      expect(tm.log.errors.length).toBe(1);
+    });
+
+    test('Test output all fail', () => {
+      const data = JSON.parse(JSON.stringify(dataTest));
+      data[0].value = 2;
+      data[1].value = 2;
+      const tm = new TestManager(data);
+      tm.run();
+      expect(tm.log.logs.length).toBe(0);
+      expect(tm.log.errors.length).toBe(2);
     });
   });
 });
